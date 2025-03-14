08a62079b0b28cb6c8bece02f92c6b2e9f7dbc63
nizamani <shehzad.nizamani@gmail.com>
2025-03-14 18:21:59 +0500

    main fixed phone field

--- src/app/components/PhoneField.tsx
+++ src/app/components/PhoneField.tsx
@@ -7,15 +7,13 @@ export default function InputField({ label }: {
     <div>
         <label className="block text-sm font-normal mb-1">{label}</label>
         <div className="flex items-center gap-2">
-        <div className="flex items-center gap-2">
         <select className="p-2 h-10 border border-gray-300 rounded-lg">
             <option>US</option>
             <option>UK</option>
             <option>CA</option>
         </select>
-            <input type="text" placeholder="+1 (555) 000-0000" className="flex-1 p-2 h-10 border border-gray-300 rounded-lg" />
+        <input type="text" placeholder="+1 (555) 000-0000" className="flex-1 p-2 h-10 border border-gray-300 rounded-lg" />
         </div>
-    </div>
     </div>
   )
 }
